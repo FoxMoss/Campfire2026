24ba8a09c1b73d962c8c936e7a2b9ddd22d677f8
FoxMoss <foxmoss@mediaology.com>
2026-03-01 11:38:35 -0600

    moving body

--- worm_body.gd
+++ worm_body.gd
@@ -2,15 +2,22 @@ extends Node2D
 
 const segments = 20
 var segments_storage = {}
+var segments_position_storage = {}
+var dist = 6*4
 
 func _ready() -> void:
 	for i in range(0, segments):
 		segments_storage[i] = $Wormbody.duplicate()
-		(segments_storage[i] as Node2D).position.x = 6*4 * i
-		(segments_storage[i] as Node2D).visible = true
+		(segments_storage[i]  as Node2D).position.x = dist * i
+		segments_position_storage[i] = (segments_storage[i] as Node2D).global_position
+		(segments_storage[i] as Node2D).visible = i != 0
 		add_child(segments_storage[i] as Node2D)
 
-
-
 func _process(delta: float) -> void:
+	(segments_storage[0] as Node2D).global_position = self.global_position
+	
+	for i in range(1, segments):
+		(segments_storage[i] as Node2D).global_position = segments_position_storage[i]
+		(segments_storage[i] as Node2D).global_position = (segments_storage[i-1] as Node2D).global_position + ((segments_storage[i] as Node2D).global_position - (segments_storage[i-1] as Node2D).global_position).normalized() * dist
+		segments_position_storage[i] = (segments_storage[i] as Node2D).global_position
 	pass
